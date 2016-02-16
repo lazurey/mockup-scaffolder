be07cc3d9a95e833f71adc2cfe4528fc9abf08df
Sause <lazureys@gmail.com>
2016-02-16 10:36:43 +0800

    update gulp file

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 - sass
 - jade
 - gulp
+
+## Support functionalities
+
+- sass compile
+- jade compile
+- copy js
+- copy image
+- multiple language folders
--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('sass', function() {
 gulp.task('templates', function() {
   var YOUR_LOCALS = {};
  
-  gulp.src('./src/templates/*.jade')
+  gulp.src(['./src/templates/**/*.jade', '!./src/templates/partials/*.jade'])
     .pipe(jade({
       locals: YOUR_LOCALS
     }))
@@ -30,9 +30,23 @@ gulp.task('templates', function() {
     .pipe(gulp.dest('./public/'))
 });
 
-gulp.task('watch', ['sass', 'templates'], function () {
+gulp.task('copy', function() {
+  gulp.src('./src/images/**')
+    .pipe(gulp.dest('./public/assets/images'));
+  gulp.src('./src/font/**')
+    .pipe(gulp.dest('./public/assets/font'));
+});
+
+gulp.task('copyjs', function() {
+  gulp.src('./src/js/**')
+    .pipe(gulp.dest('./public/assets/js'));
+});
+
+gulp.task('watch', ['sass', 'templates', 'copy', 'copyjs'], function () {
   gulp.watch('src/sass/**/*.+(scss|sass)', ['sass']);
   gulp.watch('src/templates/**/*.jade', ['templates']);
+  gulp.watch('src/js/*.js', ['copyjs']);
+  gulp.watch('src/images/**', ['copy']);
 });
 
 gulp.task('default', ['watch']);
